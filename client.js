f7d073d3379836c8f9289224b2aabc6a1b00daa2
Kurt Galvin <kurtgalvin@gmail.com>
2020-04-08 20:04:59 +0000

    add name in client

--- client.js
+++ client.js
@@ -1,3 +1,22 @@
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '50.64.116.162',
+    port: 50541
+  });
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', function() {
+    console.log('Connected');
+    conn.write('Name: KG')
+  })
+
+  conn.on('data', function(data) {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = { connect };
