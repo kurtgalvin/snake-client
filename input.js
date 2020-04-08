bb8c19444f030dbb90ed3bdc1db5fbeddf77065d
Kurt Galvin <kurtgalvin@gmail.com>
2020-04-08 21:03:50 +0000

    output input commands

--- input.js
+++ input.js
@@ -1,12 +1,29 @@
+let connection;
+
 const handleUserInput = function(data) {
   if (data === '\x03') {
     process.exit()
-  } else {
-    console.log('here', data)
+  }
+  switch (data) {
+    case "w":
+      connection.write('Move: up');
+      break;
+    case "a":
+      connection.write('Move: left');
+      break;
+    case "s":
+      connection.write('Move: down');
+      break;
+    case "d":
+      connection.write('Move: right');
+      break;
+    default:
+      break;
   }
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
